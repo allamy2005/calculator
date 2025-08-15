ef6e8d8594a9a9de4ba33136661a544c10e0001f
allamy2005 <jafrjafr756@gmail.com>
2025-08-16 02:51:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ function calculateResult() {
 document.getElementById("display").addEventListener("keydown", function(event) {
   const allowedKeys = [
     "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
-    "+", "-", "*", "/", ".", "(", ")",
+    "+", "-", "*", "/", ".", "(", ")", 
     "Backspace", "Delete", "ArrowLeft", "ArrowRight", "Enter"
   ];
 
